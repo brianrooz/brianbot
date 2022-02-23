0078f4be311c809c2552bf8bd8c99117181500d3
brianrooz <rruiz828@gmail.com>
2022-02-23 00:21:21 -0500

    make brianbot more personable

--- software/main.js
+++ software/main.js
@@ -2,17 +2,65 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
+/* greetings list */
+var robot_noises = [
+    "***bleep*** ",
+    "***bloop*** ",
+    "***beep*** ",
+    "***boop*** ",
+    "***bleep bloop*** ",
+    "***beep boop*** ",
+    "***bleep boop*** ",
+    "***beep bloop*** ",
+]
+var human_greetings = [
+    "hello ",
+    ". :slight_smile:",
+    "howdy ",
+    "! :cowboy:",
+    "greetings ",
+    ". :alien:",
+    "hi ",
+    ". :blush:",
+    "what's up, ",
+    "? :sunglasses:",
+    "how goes it, ",
+    "? :smirk:",
+    "how's it going, ",
+    "? :smile:",
+    "what up what upppp ",
+    "? :sunglasses:",
+    "what's good, ",
+    "? :grin:",
+    "hey ",
+    ". :wink:",
+    "yoooo! it's ",
+    "! :star_struck:",
+    "AHHH! ROBOT NOISES!! ",
+    "! :robot::zap::x:",
+]
+
+function get_random_number(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function get_random_greeting(username) {
+    var random_noise = get_random_number(robot_noises.length);
+    var random_greeting = 2 * (get_random_number(human_greetings.length) / 2) - 1;
+
+    return (robot_noises[random_noise] +
+        human_greetings[random_greeting] +
+        username +
+        human_greetings[random_greeting + 1])
+}
+
 client.on('ready', () => {
     console.log('brianbot is online: ' + client.user.id);
 });
 
 client.on('messageCreate', (msg) => {
-    console.log(msg)
     if ((msg.content.includes('hi')) && (msg.content.includes(client.user.id))) {
-        msg.reply('*bleep bloop* hi ' + msg.author.username + '! :)');
-    }
-    else if (!(msg.content.includes('hi')) && (msg.content.includes(client.user.id))) {
-        msg.reply('hm? did someone call me *bloop*?')
+        msg.reply(get_random_greeting(msg.author.username))
     }
 });
 
