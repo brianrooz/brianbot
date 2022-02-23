8aec9a7b42529cf0750353069b883f052c4b6da2
brianrooz <rruiz828@gmail.com>
2022-02-22 22:06:13 -0500

    add handling for direct messages to brianbot

--- software/main.js
+++ software/main.js
@@ -3,13 +3,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.on('ready', () => {
-    console.log('brianbot is online!');
+    console.log('brianbot is online: ' + client.user.id);
 });
 
 client.on('messageCreate', (msg) => {
-    console.log(msg.member.user.tag + ' said hello to brianbot!');
-    if (msg.content === '!hello') {
-        msg.reply('hi ' + msg.member.user.tag + '! :)');
+    console.log(msg)
+    if ((msg.content.includes('hi')) && (msg.content.includes(client.user.id))) {
+        msg.reply('*bleep bloop* hi ' + msg.author.username + '! :)');
+    }
+    else if (!(msg.content.includes('hi')) && (msg.content.includes(client.user.id))) {
+        msg.reply('hm? did someone call me *bloop*?')
     }
 });
 
