5bc91ee00c60b96ddd2acababe3988c7e37b0dbe
brianrooz <rruiz828@gmail.com>
2022-02-23 22:07:19 -0500

    brianbot did a little housekeep...

--- software/main.js
+++ software/main.js
@@ -1,69 +1,16 @@
-/* setup */
+/* greeting dependency setup */
+const human_greetings = require('./greetings.js').human_greetings;
+const robot_noises = require('./greetings.js').robot_noises;
+const get_random_number = require('./greetings.js').get_random_number;
+const get_random_greeting = require('./greetings.js').get_random_greeting;
+const confusion = require('./greetings.js').confusion;
+
+/* discord client setup */
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-/* greetings list */
-var robot_noises = [
-    "***bleep*** ",
-    "***bloop*** ",
-    "***beep*** ",
-    "***boop*** ",
-    "***bleep bloop*** ",
-    "***beep boop*** ",
-    "***bleep boop*** ",
-    "***beep bloop*** ",
-    "***crackle*** ",
-]
-var human_greetings = [
-    "hello ",
-    ". :slight_smile:",
-    "howdy ",
-    "! :cowboy:",
-    "greetings ",
-    ". :alien:",
-    "hi ",
-    ". :blush:",
-    "what's up, ",
-    "? :sunglasses:",
-    "how goes it, ",
-    "? :smirk:",
-    "how's it going, ",
-    "? :smile:",
-    "what up what upppp ",
-    "? :sunglasses:",
-    "what's good, ",
-    "? :grin:",
-    "hey ",
-    ". :wink:",
-    "yoooo! it's ",
-    "! :star_struck:",
-    "AHHH! ROBOT NOISES!! ",
-    "! :robot::zap::x:",
-]
-
-function get_random_number(max) {
-    return Math.floor(Math.random() * max);
-}
-
-function get_random_greeting(username) {
-    var random_noise = get_random_number(robot_noises.length);
-    var random_greeting = 2 * (get_random_number(human_greetings.length / 2));
-
-    return (robot_noises[random_noise] +
-        human_greetings[random_greeting] +
-        username +
-        human_greetings[random_greeting + 1])
-}
-
-function confusion(username) {
-    var random_noises = [get_random_number(robot_noises.length), get_random_number(robot_noises.length)];
-
-    return ("huh? " +
-        robot_noises[random_noises[0]] +
-        username +
-        ", did you call me " +
-        robot_noises[random_noises[1]] +
-        "?");
+function admire_wordle(results) {
+    console.log('ooh a wordle!');
 }
 
 client.on('ready', () => {
@@ -83,7 +30,7 @@ client.on('messageCreate', (msg) => {
     }
     else if (msg.channel.name.includes("another-channel")) {
         if (/^Wordle [1-9]{3,} [1-6]\W[1-6]/.test(msg.content)) {
-            msg.reply("ooh a wordle!");
+            admire_wordle(msg.content);
         }
     }
 });
